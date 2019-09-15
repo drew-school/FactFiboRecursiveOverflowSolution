1a048636c72a76eb590b866dc73226012209e85b
cookie-monster- <ilive4legos+github@gmail.com>
2019-09-15 09:47:54 -0500

    add header and includes

--- FactFiboRecursiveOverflowSolution.cpp
+++ FactFiboRecursiveOverflowSolution.cpp
@@ -1,9 +1,37 @@
-// need to add header and format according to dept standards
+/*
+Module Name:	Factorial Fibonacci Recursive Overflow
+Date:			9-15-2019
+Author:			Drew Pulliam
+
+Module Purpose
+Calculates factorials and Fibonacci numbers using both loops and recursive functions
+
+Module Design Description
+
+Inputs:
+None
+
+Outputs:
+Sequence of factorials and Fibonacci numbers until overflow is reached (on 32 bit numbers)
+
+*/
 
 #include "pch.h"
+
+#include <safeint.h>
+#include <chrono>
+#include <limits>
+#include <locale>
+
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+using namespace msl::utilities;
+using namespace chrono;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "Hello World!\n"; 
 }
